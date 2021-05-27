11faa754141c6496fc2882683b23483811c9e20f
Jane Doe <henning.brods@gmail.com>
2021-05-27 17:07:02 +0100

    Update atgui.cpp

--- src/ATGUI/atgui.cpp
+++ src/ATGUI/atgui.cpp
@@ -39,6 +39,8 @@ void SetupMainMenuBar()
 
 		ImGui::Selectable(XORSTR("Player List Window"), &PlayerList::showWindow, 0, ImVec2(ImGui::CalcTextSize(XORSTR("Player List Window"), nullptr, true).x, 0.0f));
 		ImGui::SameLine();
+		
+		ImGui::Selectable(XORSTR("Walk Bot Window"), &Walk::showWindow, 0, ImVec2(ImGui::CalcTextSize("Walk Bot Window", NULL, true).x, 0.0f));
 
 		ImGui::PopStyleVar();
 		ImGui::EndMainMenuBar();
@@ -80,6 +82,7 @@ void UI::SetupWindows()
 		Configs::RenderWindow();
 		Colors::RenderWindow();
 		PlayerList::RenderWindow();
+		Walk::RenderWindow();
 	}
 
 	ShowSpectators::RenderWindow();
